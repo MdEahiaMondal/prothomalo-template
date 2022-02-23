2402c669bbb2ffa5f94324fe18e049500ffc0802
git-e-001 <git-001@devxhub.com>
2022-02-23 13:08:48 +0600

    init

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      'sm': '576px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1280px',
+    },
+    container: {
+      padding: '1rem'
+    },
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
